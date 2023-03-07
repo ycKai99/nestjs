44eb01665ea6c3637e05ab9a7cb1b545f5d31472
99CKY <yckai99@gmail.com>
2023-03-07 14:33:56 +0800

    update

--- application/src/main.ts
+++ application/src/main.ts
@@ -8,9 +8,10 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT_SERVER);
   var bodyParser = require('body-parser');
   app.use(bodyParser.json({ limit: "50mb" }));
   app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+  await app.listen(process.env.PORT_SERVER);
+
 }
 bootstrap();
